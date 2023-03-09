183fb05c27473e1ed1156e8e4b518c6f16d29cfa
Max <99062843@mydavinci.nl>
2023-03-09 08:58:57 +0100

    copy before test

--- Module_01/O4_Horeca_app/horecaapp.js
+++ Module_01/O4_Horeca_app/horecaapp.js
@@ -1,3 +1,6 @@
+
+alert("Welkom in michelin star restaurant ")
+
 var Loop = true
 var inventory = {"bier":{name: "bier",
                         price: 4.50,
@@ -10,8 +13,18 @@ var inventory = {"bier":{name: "bier",
                          amount: 0}}
 
 while (Loop){
-    input = prompt("Wat wilt u bestellen? \n Onze assortiment bestaat uit : \n Bier 4,50€ p.st \n Wijn 80,50€ per vles \n fris 2,25€ p.st ").toLowerCase();
-    if (input in inventory){
-        amount = parseInt(prompt(""))
+    Input = prompt("Goede dag serveerster vandaag mijn naam is Hugo \n ,Wat wilt u bestellen? \n Onze assortiment bestaat uit : \n Bier 4,50€ p.st \n Wijn 80,50€ per vles \n fris 2,25€ p.st ").toLowerCase();
+    if (Input in inventory){
+        InputAmount = parseInt(prompt("Hoeveel vlessen van" + Input + "wilt u? "))
+        inventory[Input].amount += InputAmount
+    }
+    else if(Input in ["stop","niks","dat was alles","niks meer"]){
+        var Loop = false
+        alert("Bedankt voor uw bestellingen na het eten / drinken zal uw bonnetje ontvangen")
+    }
+    else{
+        alert("het spijt me deze hebben we niet op voorad")
     }
 }
+var bon = ("Uw bonnetje") 
+document.getElementById('result').innerHTML=bon;
