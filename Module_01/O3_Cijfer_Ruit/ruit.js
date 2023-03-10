1a8a08d586536ad3a948fc9aa6f7ccdb7a99e132
Max <99062843@mydavinci.nl>
2023-03-10 11:23:30 +0100

    much updated horeca app

--- Module_01/O3_Cijfer_Ruit/ruit.js
+++ Module_01/O3_Cijfer_Ruit/ruit.js
@@ -6,16 +6,13 @@ function diamond(number) {
                 line +='<br>'+ nummer;}
             else{
                 line+='-'+nummer;}
-            }
-            
-            }
+            }}
     for(aantal=number-1;aantal>=1;aantal--) {
         for(nummer=1;nummer<=aantal;nummer++) {
             if(nummer == 1){
                 line +='<br>'+ nummer;}
             else{
                 line+='-'+nummer;}
-            }
-    }
+            }}
     document.getElementById('result').innerHTML=line;
     }
--- Module_01/O4_Horeca_app/horecaapp.js
+++ Module_01/O4_Horeca_app/horecaapp.js
@@ -11,10 +11,11 @@ var inventory = {"bier":{name: "bier",
                  "fris":{name: "fris",
                          price: 2.25,
                          amount: 0}}
+
 while (Loop){
-    Input = prompt("Goede dag serveerster vandaag mijn naam is Hugo \n Wat wilt u bestellen? \n Onze assortiment bestaat uit : \n Bier 4,50€ p.st \n Wijn 80,50€ per vles \n fris 2,25€ p.st ").toLowerCase();
+    Input = prompt("Goede dag serveerster vandaag mijn naam is Hugo \n Wat wilt u bestellen? \n Onze assortiment bestaat uit : \n Bier 4,50€ p.st \n Wijn 80,50€ per vles \n Fris 2,25€ p.st ").toLowerCase();
     if (Input in inventory){
-        InputAmount = parseInt(prompt("Hoeveel vlessen van" + Input + "wilt u? "))
+        InputAmount = parseInt(prompt("Hoeveel vlessen van " + Input + " wilt u? "))
         inventory[Input].amount += InputAmount
     }
     else if(Input == "stop"){
@@ -25,17 +26,21 @@ while (Loop){
         alert("het spijt me deze hebben we niet op voorad")
     }
 }
-var bon = ("Uw bonnetje") 
-var bier = ("Bier 4,50 € ") + inventory.bier.amount + "x = " + Math.round(inventory.bier.amount * inventory.bier.price) + "€"
-var wijn = ("Wijn 80,50€ ") + inventory.wijn.amount + "x = " + Math.round(inventory.wijn.amount * inventory.wijn.price) + "€" 
-var fris = ("Fris 2,25€ ") + inventory.fris.amount + "x  = " + Math.round(inventory.fris.amount * inventory.fris.price) + "€"
-var totaal = ("uw totaal is : ") + Math.round(inventory.fris.amount * inventory.fris.price + inventory.wijn.amount * inventory.wijn.price + inventory.bier.amount * inventory.bier.price) + (" €")
-document.getElementById('result').innerHTML=bon;
-if (inventory.bier.amount != 0){
-    document.getElementById('bier').innerHTML=bier;}
-if (inventory.wijn.amount != 0){
-    document.getElementById('wijn').innerHTML=wijn;}
+function bonnetje2(inventory){
+    var bon = ("Uw bonnetje") 
+    var bier1  = ("Bier 4,50 € ") + inventory.bier.amount + "x = " + (inventory.bier.amount * inventory.bier.price).toFixed(2) + "€"
+    var wijn1 = ("Wijn 80,50€ ") + inventory.wijn.amount + "x = " + (inventory.wijn.amount * inventory.wijn.price).toFixed(2) + "€" 
+    var fris1 = ("Fris 2,25€ ") + inventory.fris.amount + "x  = " + (inventory.fris.amount * inventory.fris.price).toFixed(2) + "€"
+    var totaal1 = ("Uw totaal is : ") + (inventory.fris.amount * inventory.fris.price + inventory.wijn.amount * inventory.wijn.price + inventory.bier.amount * inventory.bier.price).toFixed(2) + (" €")
+    document.getElementById('result').innerHTML=bon;
+    if (inventory.bier.amount != 0){
+        document.getElementById('bier').innerHTML=bier1;}
+    if (inventory.wijn.amount != 0){
+        document.getElementById('wijn').innerHTML=wijn1;}
 
-if (inventory.fris.amount != 0){ 
-document.getElementById('fris').innerHTML=fris;}
-document.getElementById('totaal').innerHTML=totaal;
+    if (inventory.fris.amount != 0){ 
+    document.getElementById('fris').innerHTML=fris1;}
+    document.getElementById('totaal').innerHTML=totaal1;
+    document.getElementById("button").style.visibility="hidden";
+
+}
