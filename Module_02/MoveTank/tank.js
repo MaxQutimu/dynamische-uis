ac2892cc4d77d5c6670ab1707503560b8022e6a8
Max <99062843@mydavinci.nl>
2023-05-11 13:41:13 +0200

    commit

---
+++ Module_02/MoveTank/tank.js
@@ -0,0 +1,61 @@
+var image = document.getElementById("image");
+var missle = document.getElementById("missle");
+image.style.backgroundPosition = `164px 0px`;
+image.style.left = "0px"
+image.style.top = "0px"
+
+document.onkeydown = checkKey;
+image.style.transform = "rotate(90deg)"
+
+function checkKey(e) {
+	console.log("key nr = " + e.keyCode);
+    e = e || window.event;
+
+    const diff = 10;
+    const breach = 84
+
+    let position = parseInt(image.style.backgroundPosition)
+    let left = parseInt(image.style.left)
+    let top = parseInt(image.style.top)
+
+    if(e.keyCode == 32){
+        console.log("spacebar");
+        missle.style.backgroundPosition = `332px 164px`;
+        missle.style.left = image.style.left
+        missle.style.top = image.style.top
+        
+
+    } else if (e.keyCode == '38') { // up arrow
+        console.log("Up arrow");
+        image.style.transform = "rotate(0deg)"
+        top -= diff
+        image.style.top = top.toString() + 'px'
+
+        position += breach
+        image.style.backgroundPosition = position.toString() + 'px 0px'
+    } else if (e.keyCode == '40') { // down arrow
+        console.log("down arrow");
+        image.style.transform = "rotate(180deg)"
+        top += diff
+        image.style.top = top.toString() + 'px'
+
+        position += breach
+        image.style.backgroundPosition = position.toString() + 'px 0px'
+    } else if (e.keyCode == '37') { // left arrow
+        console.log("left arrow");
+        image.style.transform = "rotate(-90deg)"
+        left -= diff
+        image.style.left = left.toString() + 'px'
+
+        position -= breach
+        image.style.backgroundPosition = position.toString() + 'px 0px'
+    } else if (e.keyCode == '39') { // right arrow
+        console.log("right arrow");
+        image.style.transform = "rotate(90deg)"
+        left += diff
+        image.style.left = left.toString() + 'px'
+
+        position += breach
+        image.style.backgroundPosition = position.toString() + 'px 0px'
+    }
+}
